8b3aa599c8ab1be7d0bf95a158cddca53de04444
trusktr <trusktr@gmail.com>
2013-04-14 14:05:04 -0700

    Converted all numbers to Big() numbers using github.com/MikeMcl/big.js.

--- password/x.js
+++ password/x.js
@@ -1,33 +1,36 @@
+
+
+
 var bigPrimes = [
-    2199023255579,
-    87178291199,
-    479001599,
-    2971215073,
-    4398050705407,
-    3010349,
-    54018521,
-    370248451,
-    6643838879,
-    119218851371,
-    5600748293801,
-    1686049,
-    2922509,
-    3276509,
-    94418953,
-    321534781,
-    433494437,
-    780291637,
-    1405695061,
-    2971215073,
-    19577194573,
-    25209506681
+    Big(2199023255579),
+    Big(87178291199),
+    Big(479001599),
+    Big(2971215073),
+    Big(4398050705407),
+    Big(3010349),
+    Big(54018521),
+    Big(370248451),
+    Big(6643838879),
+    Big(119218851371),
+    Big(5600748293801),
+    Big(1686049),
+    Big(2922509),
+    Big(3276509),
+    Big(94418953),
+    Big(321534781),
+    Big(433494437),
+    Big(780291637),
+    Big(1405695061),
+    Big(2971215073),
+    Big(19577194573),
+    Big(25209506681)
 ];
 
 var charactersSet = [
             '0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f','g','h','i','j','k','l',
             'm','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J',
             'K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','~','`','!','@','#','$','%',
-            '^','&','*','(',')','_','-','+','=','[',']','\\',';',"'",',','.','/','{','}','|',':','"','<','>','?',' '
+            '^','&','*','(',')','_','-','+','=','[',']','\\',';',',','.','/','{','}','|',':','<','>','?'
         ];
 var tricks = [
             'kickflip',
@@ -50,9 +53,9 @@ var tricks = [
 
 function trickPickTimer4Action() {
     var t = trickPickTimer4Action;
-    var h = $('h1#title');
+    var h = $('h1#password');
     var rand = 0;
-    var goalNumberOfTrickPicks = 5;
+    var goalNumberOfTrickPicks = 2;
     // TODO: CONTINUE HERE: See CONTINUE HERE below.
     //var goalNumberOfFrames = 100;
     //var specialParameter = 0; //TODO: Rename this to something meaningful.
@@ -72,8 +75,9 @@ function trickPickTimer4Action() {
         t.numTricksPicked = 0;
     if (typeof t.numCharsRevealed == 'undefined')
         t.numCharsRevealed = 0;
-    if (typeof t.picked == 'undefined')
-        t.picked = pickTrick();
+    if (typeof t.picked == 'undefined') {
+        t.picked = generatePassword();
+        console.log(t.picked);}
     if (typeof t.previous == 'undefined')
         t.previous = h.text();
     if (typeof t.charsRevealed == 'undefined')
@@ -85,7 +89,7 @@ function trickPickTimer4Action() {
     if (!t.donePickingTricks && t.trickPickElapsed >= t.wait) {
         console.log('Picking a trick.');
         while (t.picked.length == t.previous.length) {
-            t.picked = pickTrick();
+            t.picked = generatePassword();
         }
         t.previous = t.picked;
 
@@ -114,7 +118,7 @@ function trickPickTimer4Action() {
         console.log('  Revealed a character.');
 
         t.numCharsRevealed += 1;
-        t.wait = Math.abs(Math.pow(t.numCharsRevealed*0.1 - t.picked.length*0.1, 4));
+        t.wait = Math.abs(Math.pow(t.numCharsRevealed*0.01 - t.picked.length*0.01, 4));
         t.charRevealElapsed = -1;
     }
     t.charRevealElapsed++;
@@ -127,11 +131,13 @@ function trickPickTimer4Action() {
             else {
                 h.text(h.text()+charactersSet[Math.floor(Math.random() * charactersSet.length)]);
             }
+            setGeneratorCenter();
         }
     }
     else {
         for (var i=0; i<t.picked.length; i++) {
             h.text(h.text()+charactersSet[Math.floor(Math.random() * charactersSet.length)]);
+            setGeneratorCenter();
         }
     }
 
@@ -161,33 +167,78 @@ function trickPickTimer4Action() {
         t.tricksPicked = 0;
         t.numTricksPicked = 0;
         t.numCharsRevealed = 0;
-        t.picked = pickTrick();
+        t.picked = generatePassword();
         t.previous = h.text();
         t.charsRevealed = [];
         t.charsNotRevealed = [];
     }
 }
 
-function pickTrick() {
-    return tricks[ Math.floor(Math.random() * tricks.length) ];
+function generateNumber() {
+    var g = generateNumber;
+    var Xn = Big(0); // This is Xn as in Xn from a Blum Blum Shub sequence.
+
+    if (typeof g.mouseTrailIndex == 'undefined')
+        g.mouseTrailIndex = 0;
+
+    // This is the first number of a discrete Blum Blum Shub sequence.
+    Xn = aTriangleB( mouseTrailX[g.mouseTrailIndex] .times( randomNumber), mouseTrailY[g.mouseTrailIndex] .times( aTriangleB(pickPrime(), pickPrime()) ) );
+    g.mouseTrailIndex++; // We loop through the array of mouse pointer positions...
+                        // Alternatively we could ask the user to move his mouse between password generations to generate a new table
+                        // so each password generation is more random. But we'll leave that for later.
+    if (g.mouseTrailIndex == mouseTrailX.length)
+        g.mouseTrailIndex = 0; // go back to the beginning of the mouse trail table.
+
+    // This loop does a Blum Blum Sequence of a randomly selected length, seeded by the time.
+    // Note: Anywhere you see Math.random(), the random number returned there is based on a time seed.
+    // Note: This generator couples JavaScript's time-based seeding with the user's random mousemovement,
+    // so we've increased the randomness many times over because no general pattern is likely to be
+    // observed from the user's mouse movement, and plus the Blum Blum Shub sequence generates numbers
+    // that are seemingly unrelated. Also, the triangle operater (see aTriangleB) introduces more
+    // randomness by helping us generate numbers derived from random arithmetic. for the various parameters
+    // of our sequence of Blum Blum Shub sequences
+    // The end result of each Blum Blum Shub sequence is used along with the user's mouse movement to
+    // generate a new starting point for the next sequence that will derive the next random number.
+    var M = pickPrime() .times( pickPrime() ); // Big() number
+    var n = Math.floor(Math.random() * 100)+50;
+    for (var i=0; i<n; i++) {
+        Xn = Xn .pow( Big(2) ) .mod( M );
+    }
+    randomNumber = Xn; // use it globally,
+    return Xn; // or take the return value.
 }
 
-function Timer(duration, action, repeat/*TODO: ,autostart*/) {
-	var timerId, paused=false, start, remaining = duration;
-	this.pause = function() {
-		paused = true;
-		window.clearTimeout(timerId);
-		remaining -= new Date() - start;
-	};
-	this.stop = function() {
-		paused = true;
-		window.clearTimeout(timerId);
-		remaining = duration; // back to the beginning.
-	};
-	this.resume = function() {
-		paused = false;
-		start = new Date();
-		if (repeat) {
+function generatePassword() {
+    var passwordLength = 24; // TODO: Allow the user to choose the password length. ;)
+    var password = "";
+    var index = Big(0);
+    var rand = Big(0);
+    for (var i=0; i<passwordLength; i++) {
+        rand = generateNumber().toFixed(0);
+        console.log(" -- rand: "+rand);
+        index = rand .mod( Big(charactersSet.length) );
+        console.log(' -- index: '+index);
+        password += charactersSet[parseInt( index.toFixed(0).toString() )];
+    }
+    return password;
+}
+
+function Timer(duration, action, repeat, autostart) {
+    var timerId, paused=false, start, remaining = duration;
+    this.pause = function() {
+        paused = true;
+        window.clearTimeout(timerId);
+        remaining -= new Date() - start;
+    };
+    this.stop = function() {
+        paused = true;
+        window.clearTimeout(timerId);
+        remaining = duration; // back to the beginning.
+    };
+    this.resume = function() {
+        paused = false;
+        start = new Date();
+        if (repeat) {
                     // TODO: The interval duration will be messed up after pausing. It needs a mechanism of resetting the remaining to the original duration.
                     timerId = window.setInterval(action, remaining);
                 }
@@ -195,23 +246,25 @@ function Timer(duration, action, repeat/*TODO: ,autostart*/) {
                     $('*').css('border', '1px solid red')
                     timerId = window.setTimeout(action, remaining);
                 }
-	};
-	this.start = function() {
-		this.resume();
-	}
-	this.isPaused = function() {
-		return paused;
-	};
-	this.setRemaining = function(newValue) {
-		remaining = newValue;
-	};
-	this.resume(); // if autostart.
+    };
+    this.start = function() {
+        this.resume();
+    }
+    this.isPaused = function() {
+        return paused;
+    };
+    this.setRemaining = function(newValue) {
+        remaining = newValue;
+    };
+        if (autostart) {
+            this.resume();
+        }
 };
 
-var trickPickTimer4 = new Timer(75, trickPickTimer4Action, true);
+var trickPickTimer4 = new Timer(75, trickPickTimer4Action, true, false);
 function randomTrick() {
     // onclick, make sure the timer is stopped, reset all vars, start over.
-    var t = trickPickTimer4Action, h = $('h1#title');
+    var t = trickPickTimer4Action, h = $('h1#password');
     trickPickTimer4.stop();
     t.donePickingTricks = false;
     t.trickPickElapsed = 0;
@@ -220,7 +273,7 @@ function randomTrick() {
     t.tricksPicked = 0;
     t.numTricksPicked = 0;
     t.numCharsRevealed = 0;
-    t.picked = pickTrick();
+    t.picked = generatePassword();
     t.previous = h.text();
     t.charsRevealed = [];
     t.charsNotRevealed = [];
@@ -228,51 +281,132 @@ function randomTrick() {
 }
 
 
-function setTitleCenter() {
-	var _title = $('#title'),
-		width = _title.width(),
-		height = _title.height();
+function setGeneratorCenter() {
+    var _gen = $('#generator'),
+        width = _gen.width(),
+        height = _gen.height();
 
-	_title.css({"margin-left" : ""+(-width/2)+"px"});
-	_title.css({"margin-top" : ""+(-height/2)+"px"});
+    _gen.css({"margin-left" : "-"+(width/2)+"px"});
+    _gen.css({"margin-top" : "-"+(height/2)+"px"});
 }
 
+function setGeneratorButtonCenter() {
+    var _button = $('#generate');
+    _button.css('margin-left','-'+(_button.outerWidth()/2)+'px');
+}
+
+function pickPrime() {
+    return bigPrimes[Math.floor(Math.random() * bigPrimes.length)];
+}
+
+/*
+ *aTriangleB takes a and B and performs one of four random arithmetic operations on it.
+ *Let's call it the triangle operator.
+ */
+function aTriangleB(a, B) {
+    var rand = Math.floor(Math.random() * 4); // a random number to choose aa random operation to perform on the two operands of aTriangleB
+    var aString = a.toString(), BString = B.toString();
+    console.log(" -- a to string: "+aString);
+    console.log(" -- B to string: "+BString);
+    var longestLength = aString.length >= BString.length ? aString.length : BString.length;
+    var subResult = Big(0);
+    var subResult_absolute = Big(0);
+    if (rand == 0) {
+        // add
+        return a .plus( B);
+    }
+    else if (rand == 1) {
+        // subtract
+        console.log(" --- a.cmp(B) test: "+a.cmp(B));
+        subResult = a .minus( B);
+        subResult_absolute = subResult.cmp(Big(0)) < 0 ? subResult.minus(subResult).minus(subResult) : subResult;
+        return subResult_absolute; // keep the result positive (absolute).
+    }
+    else if (rand == 2) {
+        // multiply
+        return a .times( B);
+    }
+    else if (rand == 3) {
+        // divide
+        return (a .div( B)) .times( Big(10) .pow( Big(longestLength) ) ); // keep the result big, not small.
+    }
+}
+
+var randomNumber = Big(0); // This contains the result each time that generateNumber() is called. This must be > 0 before calling generateNumber();
+function generateInitial() {
+    // This function calls generateNumber() a few times using the first 10 pairs of X,Y coordinates
+    // of the initial mouse movement to seed the random generator.
+    // This generator relies on the date() that the code gets executed as well as the user's mouse input.
+
+    console.log('generate initial.');
+    for (var i=0; i<10; i++) {
+        generateNumber();
+    }
+}
+
+var mouseTrailX = [];
+var mouseTrailY = [];
 $(document).ready(function() {
-	setTitleCenter();
+    setGeneratorCenter();
+    setGeneratorButtonCenter();
 
-	$('html').css({'background-color':'none'});
+    $('html').css({'background-color':'none'});
 
 
-	var mouseMoveCount = 0;
-	$('body').on('mousemove', function() {
-		var _this = $(this),
-			colorHigh = 219, // RGB
-			colorLow = 143, // RGB
-			colorRange = colorHigh - colorLow,
-			rgbParts = [];
+    var mouseMoveCount = 0;
+    var movedEnough = false;
+    $('body').on('mousemove', function(event) {
+        mouseMoveCount++;
+        console.log(mouseMoveCount);
+        var _this = $(this),
+            colorHigh = 219, // RGB
+            colorLow = 143, // RGB
+            colorRange = colorHigh - colorLow,
+            rgbParts = [];
 
-		if (mouseMoveCount%20 == 0) {
-			for (var i=0; i<3; i++) {
-				rgbParts[i] = Math.floor(Math.random()*colorRange + colorLow);
-				console.log(rgbParts[i]);
-			}
-			$('body').css({'background-color':'rgb('+rgbParts[0]+','+rgbParts[1]+','+rgbParts[2]+')'}); // TODO use integers
+        // Set random color on each mouse move.
+        if (mouseMoveCount%5 == 0) {
+            for (var i=0; i<3; i++) {
+                rgbParts[i] = Math.floor(Math.random()*colorRange + colorLow);
+            }
+            $('#password').css({'color':'rgb('+rgbParts[0]+','+rgbParts[1]+','+rgbParts[2]+')'}); // TODO use integers
 
-			mouseMoveCount = 0;
-		}
+            if (movedEnough)
+                mouseMoveCount = 0;
+        }
 
-		mouseMoveCount++;
-	});
+        // Collect initial mouse move positions to seed random.
+        if (!movedEnough) {
+            mouseTrailX.push(event.pageX);
+            mouseTrailY.push(event.pageY);
+            if (mouseMoveCount >= 150) {
+                movedEnough = true;
+                // Convert each mouse movement into Big() numbers.
+                for (var i=0; i<mouseTrailX.length; i++) {
+                    mouseTrailX[i] = Big(mouseTrailX[i]);
+                    mouseTrailY[i] = Big(mouseTrailY[i]);
+                }
+                randomNumber = pickPrime(); // randomNumber needs to be initialized one time to begin the sequence.
+                console.log('Before generateInitial.');
+                generateInitial(); // This is synchronous, so the next line enables the generate button after this line is done.
+                $('#generate').removeAttr('disabled');
+            }
+        }
+    });
 
-        $('body').on('click', function() {
+    $('#generate').on('click', function() {
+        var _this = $(this);
+        if (_this.attr('disabled') == "disbabled") {
+            // nothing.
+        }
+        else {
             randomTrick();
-        });
-
-        var 
+        }
+    });
 });
 
 $(window).resize(function(){
-    setTitleCenter();
+    setGeneratorCenter();
 });
 
 
