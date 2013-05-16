d838a62bb88e6fea0ec4ad0ff9b2c2bf6756b9b5
trusktr <trusktr@gmail.com>
2013-05-16 07:10:45 -0700

    Working on PL4. Almost done. Taking way longer than I thought. :(

--- PL4/recursive_descent.js
+++ PL4/recursive_descent.js
@@ -4,78 +4,121 @@
  *lexiScanner is a class with methods.
  */
 var lexiScanner = function() {
-    this.firstToken = function(input) {
+    var string = "";
+    var charPtr = 0; // points to the initial character at first.
+    var advanced = false;
+
+    this.setString = function(inputString) {
+        string = inputString;
+        charPtr = 0; // back to the beginning.
+    };
+
+    this.firstToken = function() {
         // get first character from string.
-        return input.charAt(0);
+        return string.charAt(0);
     };
-    this.getNextTerminal = function(input) {
-        return input.charAt(this.charPtr < input.length-1 ? this.charPtr++ : this.charPtr);
+    this.getCurrentTerminal = function() {
+        return string.charAt(charPtr);
     };
-    this.getPreviousTerminal = function(input) {
-        return input.charAt(this.charPtr > 0 ? this.charPtr-- : this.charPtr);
+    this.getNextTerminal = function() {
+        return string.charAt(charPtr < string.length-1 ? ++charPtr : charPtr);
     };
-
-    this.charPtr = 0; // points to initial char at first.
-}
-
-function advanceTokenPtr(tokenPtr) {
-    var tokenPtr = lexiScanner.getNextTerminal(inputString);
-    // lexiScanner to be written later (or not really in the case of PL Hwk 1)...
-}
-
-function exp(tokenPtr) {
-    if (!term(tokenPtr)) {
-        print "error: TERM expected.";
-        return false;
-    }
-
-    while (tokenPtr == "+" || tokenPtr == "-") {
-        advanceTokenPtr(tokenPtr);
-        if (!term(tokenPtr)) {
-            print "error: TERM expected after '+' or '-'.";
-            return false;
-        }
-    }
-
-    return true;
-}
-
-function term(inputString) {
-    if (!factor(tokenPtr)) {
-        print "error: FACTOR expected.";
-        return false;
-    }
-
-    while (tokenPtr == "*" tokenPtr == "/") {
-        advanceTokenPtr(tokenPtr);
-        if (!factor(tokenPtr)) {
-            print "error: FACTOR expected after '*' or '/'.";
-            return false;
-        }
-    }
-
-    return true;
-}
-
-
-function factor(tokenPtr) {
-    if (tokenPtr == "(") {
-        advanceTokenPtr(tokenPtr);
-        if exp(tokenPtr) {
-            if (tokenPtr != ")") {
-                print "error: ')' expected after EXP";
-                return false;
-            }
-            advanceTokenPtr(tokenPtr);
+    this.getPreviousTerminal = function() {
+        return string.charAt(charPtr > 0 ? --charPtr : charPtr);
+    };
+    this.index = function() {
+        return charPtr;
+    };
+    this.advancePtr = function() {
+        if (charPtr < string.length-1) {
+            charPtr++;
+            advanced = true;
         }
         else {
-            print "error: EXP expected after '('";
+            advanced = false;
+        }
+        return charPtr;
+    };
+    this.rewindPtr = function() {
+        if (charPtr > 0) {
+            charPtr--;
+            advanced = false;
+        }
+        else {
+            advanced = false;
+        }
+        return charPtr;
+    };
+    this.hasAdvanced = function() {
+        return advanced;
+    };
+    this.isLastChar = function() {
+        if (charPtr == string.length-1) {
+            return true;
+        }
+        return false;
+    };
+
+}
+var scanner = new lexiScanner(); // to be used globally by all functions. Yeah, this is bad, but it's good enough for now. :)
+var doneScanning = false;
+
+function exp() {
+    if (!term()) {
+        console.log("error: TERM expected.");
+        return false;
+    }
+
+    while (scanner.getCurrentTerminal() == "+" || scanner.getCurrentTerminal() == "-") {
+        scanner.advancePtr();
+        if (!term()) {
+            console.log("error: TERM expected after '+' or '-'.");
+            return false;
+        }
+    }
+
+    return true;
+}
+
+function term() {
+    if (!factor()) {
+        console.log("error: FACTOR expected.");
+        return false;
+    }
+
+    while (scanner.getCurrentTerminal() == "*" || scanner.getCurrentTerminal() == "/") {
+        scanner.advancePtr();
+        if (!factor()) {
+            console.log("error: FACTOR expected after '*' or '/'.");
+            return false;
+        }
+    }
+
+    return true;
+}
+
+
+var parensOpen = 0;
+function factor() {
+    if (scanner.getCurrentTerminal() == "(") {
+        parensOpen++;
+        if (scanner.index() == scanner.advancePtr()) return false; // there MUST be something after (, or it's invalid.
+        if (exp()) {
+            if (scanner.getCurrentTerminal() != ")") {
+                console.log("error: ')' expected after EXP");
+                return false;
+            }
+            parensOpen--;
+            scanner.advancePtr();
+        }
+        else {
+            console.log("error: EXP expected after '('");
             return false;
         }
     }
     else {
-        if !digit(tokenPtr) {
-            print "error: DIGIT expected.";
+        if (!digit()) {
+            console.log("error: DIGIT expected.");
             return false;
         }
     }
@@ -84,27 +127,67 @@ function factor(tokenPtr) {
 }
 
 
-function digit(tokenPtr) {
-    if (tokenPtr != "0" || "1" || "2" || "3") {
-        print "error: '0', '1', '2', or '3' expected.";
+function digit() {
+    if (!(
+        parseInt(scanner.getCurrentTerminal()) == 0 ||
+        parseInt(scanner.getCurrentTerminal()) == 1 ||
+        parseInt(scanner.getCurrentTerminal()) == 2 ||
+        parseInt(scanner.getCurrentTerminal()) == 3
+    )) {
+        console.log("error: '0', '1', '2', or '3' expected.");
         return false;
     }
+    var digitIndex = 0;
+        digitIndex = scanner.index();
+        console.log("Digit Index: "+digitIndex);
+        scanner.advancePtr();
+        console.log("New Index: "+scanner.index());
+        console.log("AFTER DIGIT: "+scanner.getCurrentTerminal());
 
-    advanceTokenPtr(tokenPtr);
+
+        //a digit can be only be followed by an operator, closing parenthesis if open parenthesis,
+        //or END of string.  Return false otherwise.
+        if (scanner.index() != digitIndex)
+        {
+            if (scanner.getCurrentTerminal() == "(") {
+                console.log('FOO BAR BLAH'); return false;
+            }
+        }
+        if (scanner.index() != scanner.advancePtr()) { // if there's more after a DIGIT
+            console.log('GAZOONTIGHTTTTTTTTTTTTT');
+            if (scanner.getCurrentTerminal() != "+"
+                && scanner.getCurrentTerminal() != "-"
+                && scanner.getCurrentTerminal() != "*"
+                && scanner.getCurrentTerminal() != "/"
+               )
+            {
+                console.log('ONE');
+                if (parensOpen == 0)
+                {
+                    console.log('TWO');
+                    if (scanner.getCurrentTerminal() == ")")
+                        console.log('THREE');
+                        return false;
+                }
+                if (parensOpen > 0)
+                {
+                console.log('FOUR');
+                    if (scanner.getCurrentTerminal() != ")")
+                        console.log('FIVE')
+                        return false;
+                }
+            }
+        }
+
     return true;
 }
 
 
-function recognize() {
-    var inputString = "";
-    var tokenPtr = ""; //points to characters in inputString... In reality this will be just a character.
+function recognize(inputString) {
+    scanner.setString(inputString);
+    console.log(scanner.firstToken());
 
-    console.log("Enter a string to test: ");
-    // RECEIVE INPUT into inputString... input changes on keyup of form element.
-
-    tokenPtr = lexiScanner.firstToken(inputString);
-
-    if exp(tokenPtr) {
+    if (exp()) {
         console.log("The input string is valid. Good job!");
         // show message on page.
     }
@@ -115,9 +198,14 @@ function recognize() {
 }
 
 $(document).ready(function() {
-    recognize();
+    console.log("Enter a string to test: ");
+    // RECEIVE INPUT into inputString... input changes on keyup of form element.
+    $('#string').on('keyup', function() {
+        var _this = $(this);
+        console.log("-----------------------------");
+        recognize(_this.val());
+    });
 });
-
 
 
 
--- PL4/x.js
+++ PL4/x.js
@@ -1,28 +1,67 @@
 
 var pageWidth = 0;
 
+function color_increase_brightness(hex, percent) {
+    // strip the leading # if it's there
+    hex = hex.replace(/^\s*#|\s*$/g, '');
+
+    // convert 3 char codes --> 6, e.g. `E0F` --> `EE00FF`
+    if(hex.length == 3){
+        hex = hex.replace(/(.)/g, '$1$1');
+    }
+
+    var r = parseInt(hex.substr(0, 2), 16),
+        g = parseInt(hex.substr(2, 2), 16),
+        b = parseInt(hex.substr(4, 2), 16);
+
+    return '#' +
+       ((0|(1<<8) + r + (256 - r) * percent / 100).toString(16)).substr(1) +
+       ((0|(1<<8) + g + (256 - g) * percent / 100).toString(16)).substr(1) +
+       ((0|(1<<8) + b + (256 - b) * percent / 100).toString(16)).substr(1);
+}
+
 $(document).ready(function() {
     var mouseMoveCount = 0,
         colorHigh = 359, // HSL Hue
         colorLow = 0, // HSL Hue
         colorRange = colorHigh - colorLow,
         colorHue,
-        colorSaturation = '34%',
-        colorLightness = '46%',
+        colorSaturation = 34,
+        colorLightness = 46,
         hslParts = [];
+    var _inputs = $('input[type="text"]');
 
     $('html, body').on('mousemove', function(event) {
         //Gradient HSL color change on mouse move for background and text.
         var _this = $(this);
 
         colorHue = Math.floor(event.pageX / pageWidth * colorRange + colorLow);
-        $('body, html').css({'background-color':'hsl('+colorHue+','+colorSaturation+','+colorLightness+')'});
-        $('a[href!="next"]').css({'color':'hsl('+colorHue+','+colorSaturation+','+colorLightness+')'});
+        $('body, html').css({'background-color':'hsl('+colorHue+','+colorSaturation+'%,'+colorLightness+'%)'});
+        $('a[href!="next"]').css({'color':'hsl('+colorHue+','+colorSaturation+'%,'+colorLightness+'%)'});
+
+        _inputs.each(function() {
+            var _this = $(this);
+            if (_this.is(':focus')) {
+                _this.css({'border-color':'hsla('+colorHue+','+(colorSaturation+(100-colorSaturation)*0.25)+'%,'+(colorLightness+(100-colorLightness)*0.25)+'%, 1)'});
+            }
+            else {
+                _this.css({'border-color':'rgba(255,255,255,0.35)'});
+            }
+        });
 
         if (mouseMoveCount%10 == 0) {
             mouseMoveCount = 0;
         }
         mouseMoveCount++;
+    });
+
+    _inputs.on('focus', function() {
+        var _this = $(this);
+        _this.css({'border-color':'hsla('+colorHue+','+(colorSaturation+(100-colorSaturation)*0.25)+'%,'+(colorLightness+(100-colorLightness)*0.25)+'%, 1)'});
+    });
+    _inputs.on('blur', function() {
+        var _this = $(this);
+        _this.css({'border-color':'rgba(255,255,255,0.35)'});
     });
 
 
