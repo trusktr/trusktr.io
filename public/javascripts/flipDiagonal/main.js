6b0111f4aded2c0b1caf4bcbc464bdc1f6ae10d2
Joseph Orbegoso Pea <joe@trusktr.io>
2014-10-07 03:39:57 -0700

    Use the new methods on TransitionableTransform that I requested Famous to pull.

--- public/javascripts/flipDiagonal/main.js
+++ public/javascripts/flipDiagonal/main.js
@@ -17,13 +17,25 @@ var mainNode = context.add(mainMod);
 var calendar = new Calendar([300, 300], 'flipDiagonal');
 mainNode.add(calendar.getNode());
 
+var moveTo = 0;
+function moveBackAndForth() {
+    moveTo = +!moveTo;
+    tt.setTranslateZ(moveTo*950, {duration: 6000, curve: 'easeInOut'}, moveBackAndForth);
+}
+moveBackAndForth();
+
 var rotateTo = 0;
-setInterval(f=> {
+function rotateYBackAndForth() {
     rotateTo = +!rotateTo;
-    tt.setTranslate([null,null,rotateTo*950], {duration: 6000, curve: 'easeInOut'});
-    tt.setRotate([null,rotateTo*Math.PI,null], {duration: 6300, curve: 'easeInOut'});
-    tt.setRotate([Math.PI/7,null,Math.PI/7], {duration: 3150, curve: 'easeIn'}, f=> {
-        tt.setRotate([0,null,0], {duration: 3150, curve: 'easeOut'});
-    });
-}, 1500);
+    tt.setRotateY(rotateTo*Math.PI, {duration: 6300, curve: 'easeInOut'}, rotateYBackAndForth);
+}
+rotateYBackAndForth();
+
+var tiltTo = 0;
+var tiltTransitions = ['easeOut','easeIn'];
+function tiltOverAndOver() {
+    tiltTo = +!tiltTo;
+    tt.setRotate([tiltTo*Math.PI/7,null,tiltTo*Math.PI/7], {duration: 3150, curve: tiltTransitions[tiltTo]}, tiltOverAndOver);
+}
+tiltOverAndOver();
 
