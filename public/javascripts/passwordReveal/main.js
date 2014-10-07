b04b1a13bd4fb47e0505aba6e6dcfc171f4a4d54
Joseph Orbegoso Pea <joe@trusktr.io>
2014-10-07 07:29:12 -0700

    Password reveal: Finished initial concept.

--- public/javascripts/passwordReveal/main.js
+++ public/javascripts/passwordReveal/main.js
@@ -5,12 +5,14 @@ import Transitionable from 'famous/transitions/Transitionable';
 import Easing from 'famous/transitions/Easing';
 import HeaderFooterLayout from 'famous/views/HeaderFooterLayout';
 
+import $ from 'jquery';
+
 import Plane from 'javascripts/components/Plane';
 import Molecule from 'javascripts/components/Molecule';
 import {contextWithPerspective} from 'javascripts/components/utils';
 
-var context = contextWithPerspective(1000);
-var mainMol = new Molecule();
+var context  = contextWithPerspective(1000);
+var mainMol  = new Molecule();
 var mainNode = context.add(mainMol.getNode());
 
 var resume = new Plane({
@@ -19,9 +21,16 @@ var resume = new Plane({
         "-webkit-filter": 'blur(7px)',
         "-moz-filter":    'blur(7px)',
         "-ms-filter":     'blur(7px)',
-        "-o-filter":      'blur(7px)'
+        "-o-filter":      'blur(7px)',
+        "filter":           'blur(7px)'
     },
     content: '<iframe style="width: 100%; height: 100%" src="https://docs.google.com/viewer?embedded=true&url=trusktr.io/boring_resume.pdf"></iframe>',
+});
+var isFirefox = typeof InstallTrigger == 'object'? true: false;
+var contentBlocker = new Plane({
+    properties: {
+        background: 'white'
+    }
 });
 
 var layout = new HeaderFooterLayout({
@@ -34,23 +43,22 @@ var footerMolecule = new Molecule();
 var loginLink = new Plane({
     content: '<a style="cursor: pointer">Login to view</a>',
     properties: {
-        background: '#333',
-        color: 'pink',
+        background:   '#333',
+        color:        'pink',
         paddingRight: '20px',
-        lineHeight: "50px",
-        textAlign: "right",
-        boxShadow:            '0px -30px 40px 40px rgba(0,0,0,0.8)',
+        lineHeight:   "50px",
+        textAlign:    "right",
+        boxShadow:    '0px -30px 40px 40px rgba(0,0,0,0.8)',
     }
 })
+
 layout.header.add(headerMolecule).add(loginLink.getNode());
+
 layout.content.add(resume.getNode());
-layout.content.add(new Molecule({
-    opacity: 0
-}).getNode()).add(new Plane({
-    properties: {
-        background: 'white'
-    }
-}).getNode());
+layout.content.add( new Molecule({
+    opacity: isFirefox? 1: 0
+}).getNode()).add(contentBlocker.getNode());
+
 layout.footer.add(footerMolecule).add(new Plane({
     content: "Footer",
     properties: {
@@ -69,33 +77,107 @@ var passwordBoxNode = mainNode.add(passwordBoxMol.getNode());
 
 var passwordBox = new Plane({
     size: [200,200],
+    classes: ['passwordBox'],
     content: '<h1>Login:</h1><input type="text" placeholder="username" /><br /><input type="password" placeholder="password" /><br /><button>Login</button><br />',
     properties: {
-        padding:              '10px',
-        background:           'white',
-        border:               '2px solid #ddd',
-        boxShadow:            '4px 4px 30px 0px rgba(0,0,0,0.8)',
+        display:    'none', // initially
+        padding:    '10px',
+        background: '#ddd',
+        border:     '2px solid #aaa',
+        boxShadow:  '4px 4px 30px 0px rgba(0,0,0,0.8)',
     }
 });
 passwordBoxNode.add(passwordBox.getNode());
 
 var passwordBoxVisible = false;
-loginLink.componentHandler.on('click', function() {
-    if (!passwordBoxVisible) {
-        passwordBoxVisible = true;
-        var zoom = new TransitionableTransform();
-        var fade = new Transitionable(0);
+var zoom               = new TransitionableTransform();
+var fade               = new Transitionable(0);
+var blur               = new Transitionable(7);
 
-        passwordBoxMol.componentMod.alignFrom(function() {
-            return [0.1,0.1];
+loginLink.componentHandler.on('deploy', function() {
+    // TODO: Make input and button comonents to use instead of DOM so we can handle events in Famo.us instead of with jQUery.
+    $('a').on('click', function() {
+        contentBlocker.componentSurface.setProperties({
+            display: 'block'
         });
+        passwordBox.componentSurface.setProperties({
+            display: 'block'
+        });
+        if (!passwordBoxVisible) {
+            passwordBoxVisible = true;
 
-        passwordBoxMol.componentMod.transformFrom(zoom);
-        zoom.setTranslateZ(200, {duration: 1000, curve: Easing.outExpo});
-        zoom.setRotateX(-Math.PI/4);
-        zoom.setRotateX(0, {duration: 1000, curve: Easing.outExpo});
+            passwordBoxMol.componentMod.alignFrom(function() {
+                return [0.1,0.1];
+            });
 
-        passwordBoxMol.componentMod.opacityFrom(fade);
-        fade.set(1, {duration: 1000, curve: Easing.outExpo});
-    }
+            passwordBoxMol.componentMod.transformFrom(zoom);
+            zoom.setTranslateZ(200, {duration: 1000, curve: Easing.outExpo});
+            zoom.setRotateX(-Math.PI/4);
+            zoom.setRotateX(0, {duration: 1000, curve: Easing.outExpo});
+
+            passwordBoxMol.componentMod.opacityFrom(fade);
+            fade.set(1, {duration: 1000, curve: Easing.outExpo});
+
+            var momentaryBlur = 0;
+            setTimeout(function() {
+                blur.set(7, {duration: 1000, curve: 'easeOut'});
+                var blurInterval = setInterval(function() {
+                    momentaryBlur = blur.get();
+                    resume.componentSurface.setProperties({
+                        "-webkit-filter": 'blur('+momentaryBlur+'px)',
+                        "-moz-filter":    'blur('+momentaryBlur+'px)',
+                        "-ms-filter":     'blur('+momentaryBlur+'px)',
+                        "-o-filter":      'blur('+momentaryBlur+'px)',
+                        filter:           'blur('+momentaryBlur+'px)'
+                    });
+                    if (momentaryBlur == 0) clearInterval(blurInterval);
+                }, 16);
+            }, 500);
+        }
+    });
+});
+
+passwordBox.componentSurface.on('deploy', function() {
+    // TODO: Make input and button comonents to use instead of DOM so we can handle events in Famo.us instead of with jQUery.
+    $('button').on('click', function() {
+        if ($('[type="text"]').val() == 'martian' && $('[type="password"]').val() == 'planet') {
+            contentBlocker.componentSurface.setProperties({
+                display: 'none'
+            });
+            if (passwordBoxVisible) {
+                passwordBoxVisible = false;
+
+                passwordBoxMol.componentMod.alignFrom(function() {
+                    return [0.1,0.1];
+                });
+
+                passwordBoxMol.componentMod.transformFrom(zoom);
+                zoom.setTranslateZ(0, {duration: 500, curve: Easing.inExpo});
+                zoom.setRotateX(-Math.PI/4, {duration: 500, curve: Easing.inExpo});
+
+                passwordBoxMol.componentMod.opacityFrom(fade);
+                fade.set(0, {duration: 500, curve: Easing.inExpo}, function() {
+                    passwordBox.componentSurface.setProperties({
+                        display: 'none'
+                    });
+                });
+
+                var momentaryBlur = 0;
+                setTimeout(function() {
+                    blur.set(0, {duration: 1000, curve: 'easeOut'});
+                    var blurInterval = setInterval(function() {
+                        momentaryBlur = blur.get();
+                        resume.componentSurface.setProperties({
+                            "-webkit-filter": 'blur('+momentaryBlur+'px)',
+                            "-moz-filter":    'blur('+momentaryBlur+'px)',
+                            "-ms-filter":     'blur('+momentaryBlur+'px)',
+                            "-o-filter":      'blur('+momentaryBlur+'px)',
+                            filter:           'blur('+momentaryBlur+'px)'
+                        });
+                        if (momentaryBlur == 0) clearInterval(blurInterval);
+                    }, 16);
+                }, 500);
+            }
+        }
+    });
 });
