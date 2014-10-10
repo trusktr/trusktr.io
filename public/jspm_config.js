5bcc7199e70886d4940bff592e88de062f4bfa4c
Joseph Orbegoso Pea <joe@trusktr.io>
2014-10-10 02:46:07 -0700

    Updated build steps. To build: npm install cd public ./jspm_install.sh

--- public/jspm_config.js
+++ public/jspm_config.js
@@ -8,44 +8,44 @@ System.config({
 
 System.config({
   "map": {
+    "bignumber": "github:MikeMcl/bignumber.js@^1.4.1",
+    "jsfromhell": "github:jonasraoni/JSFromHell@master",
+    "famous": "github:trusktr/famous@trusktr",
+    "big": "github:MikeMcl/big.js@^2.5.1",
+    "jquery-tooltipster": "github:iamceege/tooltipster@^3.2.6",
+    "bootstrap-datepicker": "github:eternicode/bootstrap-datepicker@^1.3.0",
     "jquery": "github:components/jquery@^2.1.1",
     "bootstrap": "github:twbs/bootstrap@^3.2.0",
     "github:twbs/bootstrap@3.2.0": {
       "jquery": "github:components/jquery@^2.1.1",
       "css": "github:systemjs/plugin-css@^0.1.0"
     },
-    "jquery-tooltipster": "github:iamceege/tooltipster@^3.2.6",
+    "github:eternicode/bootstrap-datepicker@1.3.0": {
+      "bootstrap": "github:twbs/bootstrap@^3.2.0"
+    },
     "github:iamceege/tooltipster@3.2.6": {
       "jquery": "github:components/jquery@^2.1.1",
       "css": "github:systemjs/plugin-css@^0.1.0"
     },
-    "bootstrap-datepicker": "github:eternicode/bootstrap-datepicker@^1.3.0",
-    "github:eternicode/bootstrap-datepicker@1.3.0": {
-      "bootstrap": "github:twbs/bootstrap@^3.2.0"
-    },
-    "famous": "github:trusktr/famous@master",
-    "github:trusktr/famous@master": {
+    "github:trusktr/famous@trusktr": {
       "famous-polyfills": "github:Famous/polyfills@^0.3.0",
       "css": "github:systemjs/plugin-css@^0.1.0"
-    },
-    "big": "github:MikeMcl/big.js@^2.5.1",
-    "bignumber": "github:MikeMcl/bignumber.js@^1.4.1",
-    "jsfromhell": "github:jonasraoni/JSFromHell@master"
+    }
   }
 });
 
 System.config({
   "versions": {
-    "github:systemjs/plugin-css": "0.1.0",
-    "github:components/jquery": "2.1.1",
-    "github:twbs/bootstrap": "3.2.0",
+    "github:MikeMcl/bignumber.js": "1.4.1",
+    "github:jonasraoni/JSFromHell": "master",
+    "github:trusktr/famous": "trusktr",
+    "github:MikeMcl/big.js": "2.5.1",
     "github:iamceege/tooltipster": "3.2.6",
     "github:eternicode/bootstrap-datepicker": "1.3.0",
-    "github:trusktr/famous": "master",
-    "github:Famous/polyfills": "0.3.0",
-    "github:MikeMcl/big.js": "2.5.1",
-    "github:MikeMcl/bignumber.js": "1.4.1",
-    "github:jonasraoni/JSFromHell": "master"
+    "github:components/jquery": "2.1.1",
+    "github:twbs/bootstrap": "3.2.0",
+    "github:systemjs/plugin-css": "0.1.0",
+    "github:Famous/polyfills": "0.3.0"
   }
 });
 
---
+++ public/jspm_install.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+
+jspm install
+jspm install bootstrap -o '{ "registry": "jspm", "dependencies": { "jquery": "^2.1.1", "css": "^0.1.0" }, "shim": { "js/bootstrap": { "deps": ["jquery", "../css/bootstrap.min.css!", "../css/bootstrap-theme.min.css!"], "exports": "$" }, "css/bootstrap-theme.min.css!": { "deps": ["./bootstrap.css!"] } } }'
+jspm install bootstrap-datepicker=github:eternicode/bootstrap-datepicker -o '{ "registry": "jspm", "main": "js/bootstrap-datepicker.js", "dependencies": { "bootstrap": "^3.2.0" } }'
+jspm install jquery-tooltipster=github:iamceege/tooltipster -o '{ "registry": "jspm", "main": "js/jquery.tooltipster", "shim": { "js/jquery.tooltipster": { "deps": ["jquery", "../css/tooltipster.css!"] } }, "dependencies": { "jquery": "^2.1.1", "css": "^0.1.0" } }'
+jspm install famous=github:trusktr/famous@trusktr -o '{ "registry": "jspm", "directories": { "lib": "src" }, "dependencies": { "famous-polyfills": "^0.3.0", "css": "^0.1.0" }, "shim": { "*/*": { "deps": ["famous-polyfills", "../core/famous.css!"] } } }'
+./jspm_meta_fix
