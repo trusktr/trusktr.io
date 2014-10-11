aef0db2d08bb8cd8b56bb8bd43832d788aed9854
Joseph Orbegoso Pea <joe@trusktr.io>
2014-10-11 01:41:09 -0700

    Added stylus via jspm. Doesn't work yet though.

--- public/jspm_config.js
+++ public/jspm_config.js
@@ -8,10 +8,10 @@ System.config({
 
 System.config({
   "map": {
-    "bignumber": "github:MikeMcl/bignumber.js@^1.4.1",
+    "big": "github:MikeMcl/big.js@^2.5.1",
     "jsfromhell": "github:jonasraoni/JSFromHell@master",
     "famous": "github:trusktr/famous@trusktr",
-    "big": "github:MikeMcl/big.js@^2.5.1",
+    "bignumber": "github:MikeMcl/bignumber.js@^1.4.1",
     "jquery-tooltipster": "github:iamceege/tooltipster@^3.2.6",
     "bootstrap-datepicker": "github:eternicode/bootstrap-datepicker@^1.3.0",
     "jquery": "github:components/jquery@^2.1.1",
@@ -30,22 +30,24 @@ System.config({
     "github:trusktr/famous@trusktr": {
       "famous-polyfills": "github:Famous/polyfills@^0.3.0",
       "css": "github:systemjs/plugin-css@^0.1.0"
-    }
+    },
+    "stylus": "github:LearnBoost/stylus@client"
   }
 });
 
 System.config({
   "versions": {
-    "github:MikeMcl/bignumber.js": "1.4.1",
+    "github:MikeMcl/big.js": "2.5.1",
     "github:jonasraoni/JSFromHell": "master",
     "github:trusktr/famous": "trusktr",
-    "github:MikeMcl/big.js": "2.5.1",
+    "github:MikeMcl/bignumber.js": "1.4.1",
     "github:iamceege/tooltipster": "3.2.6",
     "github:eternicode/bootstrap-datepicker": "1.3.0",
     "github:components/jquery": "2.1.1",
     "github:twbs/bootstrap": "3.2.0",
     "github:systemjs/plugin-css": "0.1.0",
-    "github:Famous/polyfills": "0.3.0"
+    "github:Famous/polyfills": "0.3.0",
+    "github:LearnBoost/stylus": "client"
   }
 });
 
