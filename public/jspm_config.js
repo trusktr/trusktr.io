5f4bcd611ad7aa23691d52a5f346ae2aab4d83f4
Joseph Orbegoso Pea <joe@trusktr.io>
2014-10-28 02:55:50 -0700

    Changed to famous-polyfills from npm. No need for the fix in jspm_meta_fix.

--- public/jspm_config.js
+++ public/jspm_config.js
@@ -1,18 +1,18 @@
 System.config({
   "paths": {
     "*": "*.js",
+    "hypership:*": "party/jspm/hypership/*.js",
     "github:*": "party/jspm/github/*.js",
-    "npm:*": "party/jspm/npm/*.js",
-    "hypership:*": "party/jspm/hypership/*.js"
+    "npm:*": "party/jspm/npm/*.js"
   }
 });
 
 System.config({
   "map": {
     "infamous": "hypership:trusktr/infamous@master",
-    "big": "github:MikeMcl/big.js@^2.5.1",
     "jsfromhell": "github:jonasraoni/JSFromHell@master",
     "bignumber": "github:MikeMcl/bignumber.js@^1.4.1",
+    "big": "github:MikeMcl/big.js@^2.5.1",
     "jquery": "github:components/jquery@^2.1.1",
     "bootstrap": "github:twbs/bootstrap@^3.2.0",
     "bootstrap-datepicker": "github:eternicode/bootstrap-datepicker@^1.3.0",
@@ -30,12 +30,15 @@ System.config({
       "jquery": "github:components/jquery@^2.1.1",
       "css": "github:systemjs/plugin-css@^0.1.0"
     },
+    "npm:famous-polyfills@0.3.0": {},
+    "npm:base64-js@0.0.4": {},
+    "npm:inherits@2.0.1": {},
+    "npm:ieee754@1.1.4": {},
+    "npm:pbkdf2-compat@2.0.1": {},
+    "npm:ripemd160@0.2.0": {},
+    "npm:Base64@0.2.1": {},
     "github:trusktr/famous@trusktr": {
-      "famous-polyfills": "github:Famous/polyfills@^0.3.0",
-      "css": "github:systemjs/plugin-css@^0.1.0"
-    },
-    "github:trusktr/famous@skew-axes": {
-      "famous-polyfills": "github:Famous/polyfills@^0.3.0",
+      "famous-polyfills": "npm:famous-polyfills@^0.3.0",
       "css": "github:systemjs/plugin-css@^0.1.0"
     }
   }
@@ -43,18 +46,27 @@ System.config({
 
 System.config({
   "versions": {
-    "github:MikeMcl/big.js": "2.5.1",
+    "hypership:trusktr/infamous": "master",
     "github:jonasraoni/JSFromHell": "master",
-    "github:trusktr/famous": "trusktr",
     "github:MikeMcl/bignumber.js": "1.4.1",
     "github:iamceege/tooltipster": "3.2.6",
+    "github:MikeMcl/big.js": "2.5.1",
+    "github:trusktr/famous": "trusktr",
     "github:eternicode/bootstrap-datepicker": "1.3.0",
+    "github:LearnBoost/stylus": "client",
     "github:components/jquery": "2.1.1",
     "github:twbs/bootstrap": "3.2.0",
     "github:systemjs/plugin-css": "0.1.0",
-    "github:Famous/polyfills": "0.3.0",
-    "github:LearnBoost/stylus": "client",
-    "hypership:trusktr/infamous": "master"
+    "npm:famous-polyfills": "0.3.0",
+    "github:jspm/nodelibs": "0.0.5",
+    "npm:base64-js": "0.0.4",
+    "npm:inherits": "2.0.1",
+    "npm:Base64": "0.2.1",
+    "npm:ieee754": "1.1.4",
+    "npm:pbkdf2-compat": "2.0.1",
+    "npm:ripemd160": "0.2.0",
+    "npm:sha.js": "2.2.6",
+    "github:systemjs/plugin-json": "0.1.0"
   }
 });
 
