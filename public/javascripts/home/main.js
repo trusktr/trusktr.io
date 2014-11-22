e829b0c69f00873665c543d02738d43a509c85c6
Joseph Orbegoso Pea <joseph.pea@climate.com>
2014-11-21 17:41:52 -0800

    Added touch functionality to the menu.

--- public/javascripts/home/main.js
+++ public/javascripts/home/main.js
@@ -3,6 +3,14 @@ import $ from 'jquery';
 import Transform from 'famous/core/Transform';
 import Transitionable from 'famous/transitions/Transitionable';
 import Easing from 'famous/transitions/Easing';
+import EventHandler from 'famous/core/EventHandler';
+import MouseSync from 'famous/inputs/MouseSync';
+import TouchSync from 'famous/inputs/TouchSync';
+import GenericSync from 'famous/inputs/GenericSync';
+    GenericSync.register({
+        mouse: MouseSync,
+        touch: TouchSync
+    });
 
 import Plane from 'infamous/Plane';
 import Molecule from 'infamous/Molecule';
@@ -57,8 +65,9 @@ function beFamous() {
 
     var context = contextWithPerspective(1000);
 
+    // TODO: use a template engine for vanilla famous. Won't have to worry about this with famous-angular or meteor-famous-views
     var menuPlane = new Plane({
-        size: [undefined,undefined],
+        size: [menuWidth + 0,undefined],
         content: ''
             +'<menu id="menu">'
                 +'<li class="menuitem" style="color: #ccc">Joe Pea</li><br />'
@@ -107,9 +116,10 @@ function beFamous() {
             +'</menu>'
         ,
         properties: {
-            zIndex: '1000',
+            zIndex: '10'
         }
     });
+    window.menuPlane = menuPlane;
 
     var framePlane = new Plane({
         size: [undefined,undefined],
@@ -139,7 +149,7 @@ function beFamous() {
     });
 
     var alignment = (menuSide == "left"? 0: 1);
-    var t = new Transitionable(0);
+    var opacityTransition = new Transitionable(0);
 
     // TODO: replace menu easing with physics, using initial velocity and drag to slow it down, and stop immediately when it hit the limit.
     function openMenu() {
@@ -148,21 +158,23 @@ function beFamous() {
         contentMol.transform.setTranslateX((menuSide == 'left'? 1: -1)*menuWidth, {duration: 1000, curve: Easing.outExpo});
         menuMol.transform.setTranslateX(0, {duration: 1000, curve: Easing.outExpo});
         contentMol.transform.setRotateY((menuSide == 'left'? 1: -1)*Math.PI/8, {duration: 1000, curve: Easing.outExpo});
-        t.halt();
-        t.set(1, {duration: 1000, curve: Easing.outExpo});
+        opacityTransition.halt();
+        opacityTransition.set(1, {duration: 1000, curve: Easing.outExpo});
         fadePlane.surface.removeClass('hidden');
     }
+    window.openMenu = openMenu;
     function closeMenu() {
         contentMol.transform.halt();
         menuMol.transform.halt();
         contentMol.transform.setTranslateX((menuSide == 'left'? 1: -1)*menuHintSize, {duration: 1000, curve: Easing.outExpo});
         menuMol.transform.setTranslateX((menuSide == 'left'? -menuWidth+menuHintSize: +menuWidth-menuHintSize), {duration: 1000, curve: Easing.outExpo});
         contentMol.transform.setRotateY(0, {duration: 1000, curve: Easing.outExpo});
-        t.halt();
-        t.set(0, {duration: 1000, curve: Easing.outExpo}, function() {
+        opacityTransition.halt();
+        opacityTransition.set(0, {duration: 1000, curve: Easing.outExpo}, function() {
             fadePlane.surface.addClass('hidden');
         });
     }
+    window.closeMenu = closeMenu;
     function toggleMenu() {
         if (typeof toggleMenu.isOpen == 'undefined') {
             toggleMenu.isOpen = false;
@@ -182,26 +194,6 @@ function beFamous() {
         contentWidth = $(window).width()-menuHintSize;
         contentMol.setOptions({size: [contentWidth, undefined]});
     }
-
-    $(window).on('resize', onWindowSizeChange);
-
-    menuPlane.on('mouseenter', function() {
-        openMenu();
-    });
-    menuPlane.on('mouseleave', function() {
-        closeMenu();
-    });
-
-    menuPlane.on('deploy', function() {
-        $('.menuitem a').on('click', function(event) {
-            var _link = $(this);
-            if (_link.parent().is('.frame')) {
-                event.preventDefault();
-                $('iframe').attr('src', _link.attr('href'));
-            }
-            else { }
-        });
-    });
 
     mainMol.setOptions({
         origin: [alignment, 0.5],
@@ -239,7 +231,7 @@ function beFamous() {
 
     framePlane.transform.setTranslateZ(-1);
     fadePlane.transform.setTranslateZ(-0.0001);
-    fadePlane.setOptions({opacity: t});
+    fadePlane.setOptions({opacity: opacityTransition});
 
     context.add(mainMol);
 
@@ -248,5 +240,89 @@ function beFamous() {
     contentMol.add(framePlane);
     contentMol.add(fadePlane);
     menuMol.add(menuPlane);
+
+    var pushAreaWidth = 20; // the area on the screen edge that the user can touch and drag to push out the menu.
+    var menuTouchPlane = new Plane({
+        size: [menuWidth + pushAreaWidth - menuHintSize, undefined],
+        properties: {
+            background: 'pink',
+            zIndex: '9' // below the menu
+        }
+    });
+    window.menuTouchPlane = menuTouchPlane;
+    // FIXME: WHY THE EFF must I also set align and origin on menuTouchPlane
+    // when I've already set it on it's parent (menuMol)?????
+    menuTouchPlane.setOptions({
+        origin: [alignment, 0.5],
+        align: [alignment, 0.5]
+    });
+    menuMol.add(menuTouchPlane);
+
+    /*
+     * EVENTS
+     */
+    var sync = new GenericSync(['touch']);
+    var handler = new EventHandler();
+
+    menuPlane.pipe(sync); // for touch events only
+    menuTouchPlane.pipe(sync);
+    sync.pipe(handler);
+
+    // TODO: combine closeMenu/openMenu with this code by instead animating a transitionable between 0 and 1 for the animation state.
+    var delta;
+    handler.on('update', function(event) { // update == drag
+        delta = event.delta;
+
+        // stop current transitions if any.
+        if (contentMol.transform.isActive()) { contentMol.transform.halt(); }
+        if (menuMol.transform.isActive()) { menuMol.transform.halt(); }
+        if (opacityTransition.isActive()) { opacityTransition.halt(); }
+
+        // move the menu, following the user's drag. Don't let the user drag the menu past the menu width.
+        // TODO: handle the right-side menu.
+        // TODO: optimize by caching function call results instead of repeating.
+        contentMol.transform.setTranslateX(Transform.getTranslate(contentMol.transform.get())[0] + delta[0]);
+        menuMol.transform.setTranslateX(Transform.getTranslate(menuMol.transform.get())[0] + delta[0]);
+        if (Transform.getTranslate(menuMol.transform.get())[0] > 0) {
+            menuMol.transform.setTranslateX(0);
+            contentMol.transform.setTranslateX(menuWidth);
+        }
+        else if (Transform.getTranslate(menuMol.transform.get())[0] < -menuWidth + menuHintSize) {
+            menuMol.transform.setTranslateX(-menuWidth + menuHintSize);
+            contentMol.transform.setTranslateX(0+menuHintSize);
+        }
+        contentMol.transform.setRotateY( (Math.PI/8) * (menuWidth + Transform.getTranslate(menuMol.transform.get())[0]) / menuWidth );
+        opacityTransition.set((menuWidth + Transform.getTranslate(menuMol.transform.get())[0]) / menuWidth);
+        fadePlane.surface.removeClass('hidden');
+    });
+    handler.on('end', function(event) {
+        // TODO: optimize by caching function call results instead of repeating.
+        if (Transform.getTranslate(menuMol.transform.get())[0] < (-menuWidth+menuHintSize)/2) {
+            closeMenu();
+        }
+        else {
+            openMenu();
+        }
+    });
+
+    menuPlane.on('mouseenter', function() {
+        openMenu();
+    });
+    menuPlane.on('mouseleave', function() {
+        closeMenu();
+    });
+
+    menuPlane.on('deploy', function() {
+        $('.menuitem a').on('click', function(event) {
+            var _link = $(this);
+            if (_link.parent().is('.frame')) {
+                event.preventDefault();
+                $('iframe').attr('src', _link.attr('href'));
+            }
+            else { }
+        });
+    });
+
+    $(window).on('resize', onWindowSizeChange);
 
 }
