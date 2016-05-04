0e2494dab44370e3491da0d67b922cf01f7d045a
Joseph Orbegoso Pea <joe@trusktr.io>
2016-05-03 17:18:22 -0700

    Restored original layout made with famous/infamous. Up next will re-write it with infamous/motor, and will hook up the routes to the demos.

--- client/main.js
+++ client/main.js
@@ -1,11 +1,31 @@
-import './routes'
-import home from './home'
+import React from 'react'
+import startup from 'awaitbox/meteor/startup'
 
 //styles
 import jss from "/client/common/jss-configured"
 import reset from './common/styles/reset'
 
+import './routes'
+
+import home from './home'
+
 // apply global reset
 jss.createStyleSheet(reset, {named: false}).attach()
 
 home()
+
+// TODO: replace root with React-based root using Motor.
+class App extends React.Component {
+    render() {
+        return (
+            <div id="blah" className="app" anything="asdf">
+                <div>1 2 3 4 5 6 </div>
+            </div>
+        )
+    }
+}
+
+async function main() {
+    await startup()
+    React.render(<App />, document.querySelector('#rootContainer'))
+}
