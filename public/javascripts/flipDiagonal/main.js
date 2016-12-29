4e627ca6bab78d15d232115fe20d7ba83c59f519
Joseph Orbegoso Pea <joe@trusktr.io>
2016-12-29 13:57:42 -0800

    Keep flipDiagonal grid in place, rotate on Y just a little bit.

--- public/javascripts/flipDiagonal/main.js
+++ public/javascripts/flipDiagonal/main.js
@@ -11,10 +11,10 @@ var context = contextWithPerspective(1000);
 var mainMol = new Molecule({
     transform: tt
 });
-tt.setTranslate([0,0,500]);
+tt.setTranslate([0,0,0]);
 
 context.add(mainMol);
-var calendar = new Calendar([300, 300], 'flipDiagonal');
+var calendar = new Calendar([768, 768], 'flipDiagonal');
 mainMol.add(calendar);
 
 var moveTo = 0;
@@ -22,12 +22,12 @@ function moveBackAndForth() {
     moveTo = +!moveTo;
     tt.setTranslateZ(moveTo*950, {duration: 6000, curve: 'easeInOut'}, moveBackAndForth);
 }
-moveBackAndForth();
+//moveBackAndForth();
 
 var rotateTo = 0;
 function rotateYBackAndForth() {
     rotateTo = +!rotateTo;
-    tt.setRotateY(rotateTo*Math.PI, {duration: 6300, curve: 'easeInOut'}, rotateYBackAndForth);
+    tt.setRotateY(rotateTo*0.3-0.3/2, {duration: 6300, curve: 'easeInOut'}, rotateYBackAndForth);
 }
 rotateYBackAndForth();
 
@@ -37,5 +37,5 @@ function tiltOverAndOver() {
     tiltTo = +!tiltTo;
     tt.setRotate([tiltTo*Math.PI/7,null,tiltTo*Math.PI/7], {duration: 3150, curve: tiltTransitions[tiltTo]}, tiltOverAndOver);
 }
-tiltOverAndOver();
+//tiltOverAndOver();
 
