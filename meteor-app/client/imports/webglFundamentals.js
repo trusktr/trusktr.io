bbda1b0ed2c45896cd1932abd76ee3c271221897
Joe Pea <joe@trusktr.io>
2017-05-21 16:42:41 -0700

    Added a camera. Basically when you move it, we're really just moving everything else in the opposite way to make it seem like there's a camera.

--- meteor-app/client/imports/webglFundamentals.js
+++ meteor-app/client/imports/webglFundamentals.js
@@ -364,6 +364,91 @@ const m4 = {
         ]
     },
 
+    inverse(m) {
+        const m00 = m[0 * 4 + 0]
+        const m01 = m[0 * 4 + 1]
+        const m02 = m[0 * 4 + 2]
+        const m03 = m[0 * 4 + 3]
+        const m10 = m[1 * 4 + 0]
+        const m11 = m[1 * 4 + 1]
+        const m12 = m[1 * 4 + 2]
+        const m13 = m[1 * 4 + 3]
+        const m20 = m[2 * 4 + 0]
+        const m21 = m[2 * 4 + 1]
+        const m22 = m[2 * 4 + 2]
+        const m23 = m[2 * 4 + 3]
+        const m30 = m[3 * 4 + 0]
+        const m31 = m[3 * 4 + 1]
+        const m32 = m[3 * 4 + 2]
+        const m33 = m[3 * 4 + 3]
+        const tmp_0  = m22 * m33
+        const tmp_1  = m32 * m23
+        const tmp_2  = m12 * m33
+        const tmp_3  = m32 * m13
+        const tmp_4  = m12 * m23
+        const tmp_5  = m22 * m13
+        const tmp_6  = m02 * m33
+        const tmp_7  = m32 * m03
+        const tmp_8  = m02 * m23
+        const tmp_9  = m22 * m03
+        const tmp_10 = m02 * m13
+        const tmp_11 = m12 * m03
+        const tmp_12 = m20 * m31
+        const tmp_13 = m30 * m21
+        const tmp_14 = m10 * m31
+        const tmp_15 = m30 * m11
+        const tmp_16 = m10 * m21
+        const tmp_17 = m20 * m11
+        const tmp_18 = m00 * m31
+        const tmp_19 = m30 * m01
+        const tmp_20 = m00 * m21
+        const tmp_21 = m20 * m01
+        const tmp_22 = m00 * m11
+        const tmp_23 = m10 * m01
+
+        const t0 = (tmp_0 * m11 + tmp_3 * m21 + tmp_4 * m31) -
+            (tmp_1 * m11 + tmp_2 * m21 + tmp_5 * m31)
+        const t1 = (tmp_1 * m01 + tmp_6 * m21 + tmp_9 * m31) -
+            (tmp_0 * m01 + tmp_7 * m21 + tmp_8 * m31)
+        const t2 = (tmp_2 * m01 + tmp_7 * m11 + tmp_10 * m31) -
+            (tmp_3 * m01 + tmp_6 * m11 + tmp_11 * m31)
+        const t3 = (tmp_5 * m01 + tmp_8 * m11 + tmp_11 * m21) -
+            (tmp_4 * m01 + tmp_9 * m11 + tmp_10 * m21)
+
+        const d = 1.0 / (m00 * t0 + m10 * t1 + m20 * t2 + m30 * t3)
+
+        return [
+            d * t0,
+            d * t1,
+            d * t2,
+            d * t3,
+            d * ((tmp_1 * m10 + tmp_2 * m20 + tmp_5 * m30) -
+                (tmp_0 * m10 + tmp_3 * m20 + tmp_4 * m30)),
+            d * ((tmp_0 * m00 + tmp_7 * m20 + tmp_8 * m30) -
+                (tmp_1 * m00 + tmp_6 * m20 + tmp_9 * m30)),
+            d * ((tmp_3 * m00 + tmp_6 * m10 + tmp_11 * m30) -
+                (tmp_2 * m00 + tmp_7 * m10 + tmp_10 * m30)),
+            d * ((tmp_4 * m00 + tmp_9 * m10 + tmp_10 * m20) -
+                (tmp_5 * m00 + tmp_8 * m10 + tmp_11 * m20)),
+            d * ((tmp_12 * m13 + tmp_15 * m23 + tmp_16 * m33) -
+                (tmp_13 * m13 + tmp_14 * m23 + tmp_17 * m33)),
+            d * ((tmp_13 * m03 + tmp_18 * m23 + tmp_21 * m33) -
+                (tmp_12 * m03 + tmp_19 * m23 + tmp_20 * m33)),
+            d * ((tmp_14 * m03 + tmp_19 * m13 + tmp_22 * m33) -
+                (tmp_15 * m03 + tmp_18 * m13 + tmp_23 * m33)),
+            d * ((tmp_17 * m03 + tmp_20 * m13 + tmp_23 * m23) -
+                (tmp_16 * m03 + tmp_21 * m13 + tmp_22 * m23)),
+            d * ((tmp_14 * m22 + tmp_17 * m32 + tmp_13 * m12) -
+                (tmp_16 * m32 + tmp_12 * m12 + tmp_15 * m22)),
+            d * ((tmp_20 * m32 + tmp_12 * m02 + tmp_19 * m22) -
+                (tmp_18 * m22 + tmp_21 * m32 + tmp_13 * m02)),
+            d * ((tmp_18 * m12 + tmp_23 * m32 + tmp_15 * m02) -
+                (tmp_22 * m32 + tmp_14 * m02 + tmp_19 * m12)),
+            d * ((tmp_22 * m22 + tmp_16 * m02 + tmp_21 * m12) -
+                (tmp_20 * m12 + tmp_23 * m22 + tmp_17 * m02))
+        ]
+    },
+
     // Note: This matrix flips the Y axis so that 0 is at the top.
     projection(width, height, depth) {
         // longer version, multiple matrices
@@ -605,6 +690,9 @@ function webglFundamentals() {
     let   yRotationMatrix   = m4.yRotation(angle.theta)
     const translationMatrix = m4.translation(0, 0, 0)
 
+    window.cameraAngle = 0
+    window.camRadius   = 500
+
     let projectionMatrix
 
     updateResolution()
@@ -631,9 +719,7 @@ function webglFundamentals() {
 
     const matrixLocation = gl.getUniformLocation(program, "matrix")
 
-    chooseRandomColors()
-
-    window.zpos = -1000
+    window.zpos = 0
     ~function draw(time) {
         tween.update(time)
 
@@ -646,11 +732,24 @@ function webglFundamentals() {
         let matrix = m4.identity
         matrix = m4.multiply(matrix, projectionMatrix)
 
+        cameraAngle++
+        let cameraMatrix  = m4.identity
+        cameraMatrix      = m4.multiply(cameraMatrix, m4.yRotation(cameraAngle))
+        cameraMatrix      = m4.multiply(cameraMatrix, m4.translation(0, 0, camRadius * 1.5))
+        const viewMatrix  = m4.inverse(cameraMatrix)
+
+        // viewProjectionMatrix
+        matrix = m4.multiply(matrix, viewMatrix)
+
+        // Node (root object)
+        //
         // place everything where we want it near the center. the new
         // projectionMatrix puts the X andY origin in the center of the screen,
         // and Z is 0 at the screen and goes  negative away from the screen.
-        matrix = m4.multiply(matrix, m4.translation(-50, 0, window.zpos))
+        matrix = m4.multiply(matrix, m4.translation(0, 0, zpos))
 
+        // Node > Node
+        //
         // matrix math is written in the opposite direction now, so that we can
         // apply the previous projection matrix only once, before all
         // drawArrays calls. For each matrix applied, think of them as happening
@@ -662,10 +761,10 @@ function webglFundamentals() {
         matrix = m4.multiply(matrix, originMatrix)
         gl.uniformMatrix4fv(matrixLocation, false, matrix)
 
-        gl.bufferData(gl.ARRAY_BUFFER, vertexColors, gl.STATIC_DRAW)
         gl.drawArrays(gl.TRIANGLES, offset, count)
 
         for (let i = 0; i < 5; ++i) {
+            // Node > Node > Node
             matrix = m4.multiply(matrix, translationMatrix)
             matrix = m4.multiply(matrix, zRotationMatrix)
             matrix = m4.multiply(matrix, yRotationMatrix)
@@ -673,7 +772,6 @@ function webglFundamentals() {
             matrix = m4.multiply(matrix, originMatrix)
             gl.uniformMatrix4fv(matrixLocation, false, matrix)
 
-            gl.bufferData(gl.ARRAY_BUFFER, vertexColors, gl.STATIC_DRAW)
             gl.drawArrays(gl.TRIANGLES, offset, count)
         }
 
