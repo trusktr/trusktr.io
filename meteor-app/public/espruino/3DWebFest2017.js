cfb198a6999b6d68ec287f45fff15eead5ba8ecc
Joe Pea <joe@trusktr.io>
2017-06-12 12:19:30 -0600

    Update espruino 3dwebfest code, but decided won't use espruino this time.

--- meteor-app/public/espruino/3DWebFest2017.js
+++ meteor-app/public/espruino/3DWebFest2017.js
@@ -23,45 +23,75 @@ function connectToWifi(wifiName, options) {
     return promise
 }
 
-connectToWifi("CenturyLink6344", { password : "zpx2psajg7xqat" })
-.then(function() {
-    const mpu = getMPU();
-    //const dmp = require('MPU6050_DMP').create(mpu, 3);
-    const http = require("http");
-
-    //let dmpYawPitchRoll = null
-
-    // This loop locks up the espruino: https://github.com/espruino/EspruinoDocs/issues/367
-    //function dmpLoop() {
-        //const dmpData = dmp.getData()
-        //if (dmpData !== undefined) console.log(dmpYawPitchRoll = dmp.getYawPitchRoll(dmpData))
-    //}
-    //setWatch(dmpLoop, B5, { repeat:true, edge:'rising' });
-
-    http.createServer(function (req, res) {
-        res.writeHead(200);
-        let result = {
-            acceleration: mpu.getAcceleration(), // returns an [x,y,z] array with raw accl. data
-            gravity: mpu.getGravity(),  // returns acceleration array in G's
-            rotation: mpu.getRotation(), // returns an [x,y,z] array with raw gyro data
-            degreesPerSecond: mpu.getDegreesPerSecond(), // returns gyro array in degrees/s
-        }
-        result = JSON.stringify(result)
-        res.end(result);
-
-        // This never works, it only seems to work in the above loop that locks up espruino.
-        //const dmpData = dmp.getData()
-        //if (dmpData !== undefined) dmpYawPitchRoll = dmp.getYawPitchRoll(dmpData)
-        //console.log(' ------ dmpYawPitchRoll:', dmpYawPitchRoll)
-
-    }).listen(80);
-
+function getIP() {
     const wifi = require("EspruinoWiFi");
     wifi.getIP(function(err, info) {
-        if (err) throw err
-
+        if (err) {
+            console.log('Wifi connection failed, trying again in a sec...')
+            setTimeout(getIP, 1000)
+        }
         console.log('IP:', info.ip)
     });
+}
+
+connectToWifi("starlancer", { password : "Next stop: Mars." })
+.then(function() {
+    getIP();
+
+    //////////////////////////////////////////////////////////////////////////////////
+    //const dmp = require('MPU6050_DMP').create(mpu, 3);
+    //let dmpYawPitchRoll = null
+    //// This loop locks up the espruino: https://github.com/espruino/EspruinoDocs/issues/367
+    //function dmpLoop() {
+        //const dmpData = dmp.getData()
+        //if (dmpData !== undefined) {
+            ////console.log(
+                //dmpYawPitchRoll = dmp.getYawPitchRoll(dmpData)
+            ////)
+        //}
+    //}
+    //setWatch(dmpLoop, B5, { repeat:true, edge:'falling' });
+
+    //const http = require("http");
+    //http.createServer(function (req, res) {
+        //const mpu = getMPU();
+        //res.writeHead(200);
+        //let result = {
+            //acceleration: mpu.getAcceleration(), // returns an [x,y,z] array with raw accl. data
+            //gravity: mpu.getGravity(),  // returns acceleration array in G's
+            //rotation: mpu.getRotation(), // returns an [x,y,z] array with raw gyro data
+            //degreesPerSecond: mpu.getDegreesPerSecond(), // returns gyro array in degrees/s
+        //}
+        //result = JSON.stringify(result)
+        //res.end(result);
+
+        //// This never fires
+        ////console.log(' ------ dmpYawPitchRoll:', dmpYawPitchRoll)
+
+    //}).listen(80);
+
+    //////////////////////////////////////////////////////////////////////////////////
+    var server = require('ws').createServer(function() {});
+
+    server.listen(80);
+    server.on("websocket", function(ws) {
+        ws.on('message', function(msg) {
+            print("[WS] "+JSON.stringify(msg));
+        });
+        const mpu = getMPU();
+        setInterval(function() {
+            let result = {
+                //acceleration: mpu.getAcceleration(), // returns an [x,y,z] array with raw accl. data
+                acceleration: mpu.getGravity(),  // returns acceleration array in G's
+                //rotation: mpu.getRotation(), // returns an [x,y,z] array with raw gyro data
+                rotation: mpu.getDegreesPerSecond(), // returns gyro array in degrees/s
+            }
+            result = JSON.stringify(result)
+            console.log(result)
+            ws.send(result);
+        }, 40);
+    });
+
 });
 
 function onInit() {
