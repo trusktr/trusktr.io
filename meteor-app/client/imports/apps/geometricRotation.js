0620726b7201ca11f72c5b36a11b3643ac799e5b
Joe Pea <joe@trusktr.io>
2017-06-17 16:52:37 -0600

    Fix geometricRotation, mesh attribute is required.

--- meteor-app/client/imports/apps/geometricRotation.js
+++ meteor-app/client/imports/apps/geometricRotation.js
@@ -26,39 +26,42 @@ class App extends React.Component {
         return (
             <motor-scene ref="scene" webglenabled="true">
                 <motor-node
-                    ref='node1'
-                    id='node1'
+                    ref='outer'
+                    id='outer'
                     absoluteSize='100, 100'
-                    //rotation='30, 30, 30'
-                    //align='0.5, 0.5'
-                    //mountPoint='0.5, 0.5'
                 >
-                    {this.positions.map((pos, index) => (
-                        <motor-node
-                            ref='node2'
-                            id='node2'
-                            key={index}
-                            absoluteSize='10, 10'
-                            //rotation={`${index*30} ${index*20} ${index*10}`}
-                            //align='0.5, 0.5'
-                            //mountPoint='0.5, 0.5'
-                            position={`${pos.x} ${pos.y} ${pos.z}`}
-                        >
-                        </motor-node>
-                    ))}
+                    <motor-node
+                        ref='inner'
+                        id='inner'
+                        absoluteSize='100, 100'
+                    >
+                        {this.positions.map((pos, index) => (
+                            <motor-node
+                                ref='node2'
+                                id='node2'
+                                key={index}
+                                absoluteSize='10, 10'
+                                //rotation={`${index*30} ${index*20} ${index*10}`}
+                                position={`${pos.x} ${pos.y} ${pos.z}`}
+                                color='0.6, 0.2, 0.3'
+                                mesh="cube"
+                            >
+                            </motor-node>
+                        ))}
+                    </motor-node>
                 </motor-node>
             </motor-scene>
         )
     }
 
     async componentDidMount() {
-        const {node1} = this.refs
-        await node1.mountPromise
-        node1.rotation.y = -90
+        const {inner} = this.refs
+        await inner.mountPromise
+        inner.rotation.y = -90
         Motor.addRenderTask(time => {
-            node1.rotation.x += 5
-            node1.rotation.y += 10
-            node1.rotation.z += 5
+            inner.rotation.x += 5
+            inner.rotation.y += 10
+            inner.rotation.z += 5
         })
     }
 }
