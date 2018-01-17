3a1136047b05e67c3d114307ce7e9d22c62b9c85
Joe Pea <joe@trusktr.io>
2018-01-16 17:54:47 -0800

    update node_modules

--- meteor-app/node_modules/infamous/node_modules/@awaitbox/document-ready/src/index.js
+++ meteor-app/node_modules/infamous/node_modules/@awaitbox/document-ready/src/index.js
@@ -5,15 +5,14 @@
  * The _passThrough arg is not for public use, it's for making data pass
  * through in promise chains.
  */
-export default
-async function documentReady( _passThrough ) {
+module.exports = function documentReady( _passThrough ) {
 
     if ( document.readyState === 'loading' ) {
-        await new Promise( resolve => {
-            document.addEventListener( 'DOMContentLoaded', resolve )
+        return new Promise( resolve => {
+            document.addEventListener( 'DOMContentLoaded', () => resolve( _passThrough ) )
         } )
     }
 
-    return _passThrough
+    return Promise.resolve( _passThrough )
 
 }
