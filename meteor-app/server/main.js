14ab4dc89a544fdc27c82bef541396ed4a3a2b27
Joe Pea <joe@trusktr.io>
2018-01-18 09:21:11 -0800

    disabled unused stuff in the server, stuff that is used for local experiments but not for the website

--- meteor-app/server/main.js
+++ meteor-app/server/main.js
@@ -13,8 +13,8 @@ import {Picker} from 'meteor/meteorhacks:picker'
 //import {BuildTools} from 'meteor/rocket:build-tools'
 import {MeteorFilesHelpers} from 'meteor/sanjo:meteor-files-helpers'
 import routes from './routes'
-import {HTTP} from 'meteor/http'
-import {parseEspruinoJson} from '/both/imports/espruino'
+//import {HTTP} from 'meteor/http'
+//import {parseEspruinoJson} from '/both/imports/espruino'
 
 const {getAppPath} = MeteorFilesHelpers
 
@@ -35,89 +35,89 @@ function sleep(duration) {
     return new Promise(r => setTimeout(r, duration))
 }
 
-function createPromise() {
-    let resolve, reject
-    const promise = new Promise((res, rej) => { resolve = res; reject = rej})
-    return {promise, resolve, reject}
-}
+//function createPromise() {
+    //let resolve, reject
+    //const promise = new Promise((res, rej) => { resolve = res; reject = rej})
+    //return {promise, resolve, reject}
+//}
 
-const http = {
-    get(url, options) {
-        const {resolve, reject, promise} = createPromise()
+//const http = {
+    //get(url, options) {
+        //const {resolve, reject, promise} = createPromise()
 
-        HTTP.call('get', url, options, (err, result) => {
-            if (err) throw err
-            resolve(result)
-        })
+        //HTTP.call('get', url, options, (err, result) => {
+            //if (err) throw err
+            //resolve(result)
+        //})
 
-        return promise
-    },
-}
+        //return promise
+    //},
+//}
 
 let previousResult
 
-Meteor.methods({
+//Meteor.methods({
 
-    async getMpuData() {
-        await sleep(1000)
-        let error = null
+    //async getMpuData() {
+        //await sleep(1000)
+        //let error = null
 
-        console.log('sending HTTP request to Espruino.')
+        //console.log('sending HTTP request to Espruino.')
 
-        let startTime = Date.now()
-        const newResult = await http.get('http://192.168.43.247')
-            .catch(e => error = e)
-        let endTime = Date.now()
+        //let startTime = Date.now()
+        //const newResult = await http.get('http://192.168.43.247')
+            //.catch(e => error = e)
+        //let endTime = Date.now()
 
-        console.log('Got HTTP response from Espruino.', endTime - startTime)
+        //console.log('Got HTTP response from Espruino.', endTime - startTime)
 
-        // if there was a network error, just use the previous result.
-        if (!error) previousResult = newResult
+        //// if there was a network error, just use the previous result.
+        //if (!error) previousResult = newResult
 
-        return parseEspruinoJson(previousResult.content)
-    },
+        //return parseEspruinoJson(previousResult.content)
+    //},
 
-    foo(...args) {
-        console.log(' --- foo method:', ...args)
-        return {bar: 'baz', n:2}
-    },
+    //foo(...args) {
+        //console.log(' --- foo method:', ...args)
+        //return {bar: 'baz', n:2}
+    //},
 
-})
+//})
 
-import MeteorClient from './imports/MeteorClient'
+//import MeteorClient from './imports/MeteorClient'
 
-async function websocketDDPTest() {
-    const client = new MeteorClient
+//async function websocketDDPTest() {
+    //const client = new MeteorClient
 
-    try {
-        const sessionId = await client.connect(
-            // find the IP in Chrome OS network settings, click on the network
-            // in the list and then "connection" tab.
-            //'192.168.0.5:3000'
-            'localhost:3000'
-        )
+    //try {
+        //const sessionId = await client.connect(
+            //// find the IP in Chrome OS network settings, click on the network
+            //// in the list and then "connection" tab.
+            ////'192.168.0.5:3000'
+            //'localhost:3000'
+        //)
 
-        console.log('session ID:', sessionId)
+        //console.log('session ID:', sessionId)
 
-        const result = await client.call('foo', 1, 2, 3)
+        //const result = await client.call('foo', 1, 2, 3)
 
-        console.log(' --------------------- Method result!:', result)
+        //console.log(' --------------------- Method result!:', result)
 
-    }
-    catch(e) {
-        throw e
-    }
-}
+    //}
+    //catch(e) {
+        //throw e
+    //}
+//}
 
-function createBroadcastServer() {
-    //let broadcast1 = new Meteor.Broadcast('orientation1')
-    //let broadcast2 = new Meteor.Broadcast('orientation2')
-    //let broadcast3 = new Meteor.Broadcast('orientation3')
+//function createBroadcastServer() {
+    ////let broadcast1 = new Meteor.Broadcast('orientation1')
+    ////let broadcast2 = new Meteor.Broadcast('orientation2')
+    ////let broadcast3 = new Meteor.Broadcast('orientation3')
 
-    let streamer = new Meteor.Streamer('orientation')
-    streamer.allowRead('all');
-    streamer.allowWrite('all');
-}
+    //let streamer = new Meteor.Streamer('orientation')
+    //streamer.allowRead('all');
+    //streamer.allowWrite('all');
+//}
 
 function setupCORS() {
     Picker.route(`(.*)`, function(params, req, res, next) {
@@ -128,7 +128,7 @@ function setupCORS() {
 
 async function main() {
     //websocketDDPTest()
-    createBroadcastServer()
+    //createBroadcastServer() // For devices to broadcast accelerometer data
     setRoutes()
     setupCORS()
     await sleep(1000)
