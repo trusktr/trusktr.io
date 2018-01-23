56cd93d5289827997a706e3bc95de6e01a730ed7
Joe Pea <joe@trusktr.io>
2018-01-22 23:48:48 -0800

    make the entry point leaner

--- meteor-app/client/main.js
+++ meteor-app/client/main.js
@@ -8,41 +8,23 @@ import { detectIE, showNeedsBetterBrowser } from './imports/utils'
         return
     }
 
-    const {default:home} = await import('./imports/home')
-    Meteor.startup(home)
+    const {default:run} = await import(
+        //'./imports/home'
+        //'./imports/earthDefense'
+        //'./imports/lettersToGrid'
+        //'./imports/motorPushMenuDev'
+        //'./imports/kanban'
+        './imports/planet'
 
-    //import earthDefense from './imports/earthDefense'
-    //Meteor.startup(earthDefense)
+        //'./imports/testElementRemoval'
+        //// currently fails
+        //'./imports/testSceneUnmount'
+        //// TODO
+        //'./imports/testShadowDomUsage'
+        //'./imports/testHtmlRerendering'
+        //'./imports/testHtmlRerendering2'
+        //'./imports/testSceneCreation'
+    )
 
-    //import lettersToGrid from './imports/lettersToGrid'
-    //Meteor.startup(lettersToGrid)
-
-    //import motorPushMenuDev from './imports/motorPushMenuDev'
-    //Meteor.startup(motorPushMenuDev)
-
-    //const {default:kanban} = await import('./imports/kanban')
-    //Meteor.startup(kanban)
-
-
-
-
-    //import testElementRemoval from './imports/testElementRemoval'
-    //Meteor.startup(testElementRemoval)
-
-    // currently fails
-    //import testSceneUnmount from './imports/testSceneUnmount'
-    //Meteor.startup(testSceneUnmount)
-
-    // TODO
-    //import testShadowDomUsage from './imports/testShadowDomUsage'
-    //Meteor.startup(testShadowDomUsage)
-
-    //import testHtmlRerendering from './imports/testHtmlRerendering'
-    //Meteor.startup(testHtmlRerendering)
-
-    //import testHtmlRerendering2 from './imports/testHtmlRerendering2'
-    //Meteor.startup(testHtmlRerendering2)
-
-    //import testSceneCreation from './imports/testSceneCreation'
-    //Meteor.startup(testSceneCreation)
+    Meteor.startup(run)
 }()
