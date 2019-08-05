dd3519927d1afe0349e91659ed2d3f92eac88a51
Joe Pea <joe@trusktr.io>
2019-08-05 02:08:22 -0700

    format reset.js

--- meteor-app/client/imports/common/styles/reset.js
+++ meteor-app/client/imports/common/styles/reset.js
@@ -1,40 +1,39 @@
-
 /* global reset */
 export default {
-    'html, body': {
-        padding: 0,
-        margin: 0,
-        height: '100%',
-        width: '100%',
-        position: 'relative',
-        backgroundColor: 'rbga(0,0,0,0)',
-    },
-    html: {
-        '& body': {
-            font: '14px "Lucida Grande", Helvetica, Arial, sans-serif',
-            //transform: translateZ(1px), [>antialiasing in firefox<]
-            overflow: 'hidden',
+	'html, body': {
+		padding: 0,
+		margin: 0,
+		height: '100%',
+		width: '100%',
+		position: 'relative',
+		backgroundColor: 'rbga(0,0,0,0)',
+	},
+	html: {
+		'& body': {
+			font: '14px "Lucida Grande", Helvetica, Arial, sans-serif',
+			//transform: translateZ(1px), [>antialiasing in firefox<]
+			overflow: 'hidden',
 
-            '& *': {
-                //remove OS focus outlines
-                //antialiasing in firefox?
-                'outline': '1px solid transparent',
+			'& *': {
+				//remove OS focus outlines
+				//antialiasing in firefox?
+				outline: '1px solid transparent',
 
-                'boxSizing': 'border-box',
-                //'pointerEvents': 'none',
-            },
+				boxSizing: 'border-box',
+				//'pointerEvents': 'none',
+			},
 
-            '& input': {
-                'border': 'none',
-            },
+			'& input': {
+				border: 'none',
+			},
 
-            '& a': {
-                //color: inherit,
-            },
+			'& a': {
+				//color: inherit,
+			},
 
-            '& iframe': {
-                'border': 'none',
-            }
-        }
-    }
+			'& iframe': {
+				border: 'none',
+			},
+		},
+	},
 }
