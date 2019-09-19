a48b51cf4e436c221feef26df824011077dccc32
Joe Pea <joe@trusktr.io>
2019-09-18 22:13:53 -0700

    add empty wedding route/page

---
+++ meteor-app/client/imports/apps/wedding.vue
@@ -0,0 +1,17 @@
+<template>
+	<h1>wedding</h1>
+</template>
+
+<style scoped>
+</style>
+
+<script>
+	export default {
+		mounted() {
+			console.log(
+				" ######################### WEDDING ################################"
+			);
+		},
+		destroyed() {}
+	};
+</script>
